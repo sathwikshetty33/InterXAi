8a930ad4abbdc3ad8e749eca44a95c01c1414a55
Sakshi146-eng <sakshishetty146@gmail.com>
2025-07-11 23:55:08 +0530

    Organization and Interview

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,9 @@ import Login from '../pages/Login';
 import Signup from '../pages/SignUp';
 import UserDashboard from '../pages/UserDashboard';
 import ProfileSetup from '../pages/ProfileSetup';
+import OrgSignup from '../pages/OrgSignup'
+import OrgDashboard from '../pages/OrgDashboard';
+import Interview from '../pages/Interview';
 
 export default function AppRoutes() {
   return (
@@ -15,6 +18,11 @@ export default function AppRoutes() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/profile/:id" element={<UserDashboard />} />
       <Route path="/profile-setup" element={<ProfileSetup />} />
+      <Route path="/register-org" element={<OrgSignup />} />
+      <Route path="/org-dashboard/:id" element={<OrgDashboard />} />
+      <Route path="/interview" element={<Interview />} />
+      
+
       {/* Add more routes as needed */}
     
     </Routes>
