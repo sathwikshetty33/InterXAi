504c654823f68c2150634237ead704ae96db24cf
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2025-08-12 17:17:57 +0000

    updated endpoints

--- core/interview/urls.py
+++ core/interview/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path('interview-session-initializer/<int:id>/', views.InterviewSessionInitializerView.as_view(), name='interview_session_initializer'),
     path('leaderboard/<int:id>/',views.LeaderBoardView.as_view(), name='leaderboard'),
     path('cheated/<int:id>/',views.CheatingDetection.as_view(),name="cheating_updater"),
-    path('get-dsa-questions/<int:id>/', views.SessionDsaQuestions.as_view(), name='get_dsa_questions')
+    path('get-dsa-questions/<int:id>/', views.SessionDsaQuestions.as_view(), name='get_dsa_questions'),
+    path('add-dsa-scores/<int:id>/<int:dsa_id>',views.SessionDsaQuestions.as_view(), name='add_dsa_scores'),
+
 ]
