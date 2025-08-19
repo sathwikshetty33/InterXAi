dafb52c3cf89af33e689b08a4bee2e83af67e0d2
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2025-08-19 12:50:47 +0000

    modified csrf origins

--- core/core/settings.py
+++ core/core/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['interxai.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['http://*','https://*']
+ALLOWED_HOSTS = ["interxai.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ['http://*','https://*',"https://interxai.onrender.com"]
 
 
 # Application definition
